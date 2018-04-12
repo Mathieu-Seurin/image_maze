719b017ff948002a19fa070669444d5c96edca95
Seurin Mathieu <matseurin@cegetel.net>
2018-04-12 15:56:44 +0200

    IMAGE STANDARD c,h,w (pytorch format) adding tools to change between format

---
+++ src/image_utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def to_rgb_channel_first(im):
+    w, h = im.shape
+    ret = np.empty((3, w, h), dtype=np.float32)
+    ret[0, :, :] = im / 255
+    ret[1, :, :] = ret[2, :, :] = ret[0, :, :]
+    return ret
+
+def to_rgb_channel_last(im):
+    w, h = im.shape
+    ret = np.empty((w, h, 3), dtype=np.float32)
+    ret[:, :, 0] = im / 255
+    ret[:, :, 1] = ret[:, :, 2] = ret[:, :, 0]
+    return ret
+
+def channel_first_to_channel_last(im):
+    assert im.shape[0] == 3, "Image needs to be in format (3,H,W)"
+    # swap from c, h, w => h,w,c
+    im = im.swapaxes(0, 2)
+    im = im.swapaxes(0, 1)
+    return im
+
+def channel_last_to_channel_first(im):
+    assert im.shape[2] == 3, "Image needs to be in format (H,W,3)"
+    # swap from h,w,c => c,h,w
+    im = im.swapaxes(0, 2)
+    im = im.swapaxes(1, 2)
+    return im
+
+def plot_single_image(im):
+    pass
+
+
+if __name__ == "__main__":
+
+    # Create image with channel being the first dimension
+    # Then swap for the last
+    # =================================================
+    one_channel_im = np.random.random((30,28))# not square to test if line and col are being kept in order
+    rgb_im_first = to_rgb_channel_first(one_channel_im)
+    assert rgb_im_first.shape[0] == 3, f"wrong channel number \nis :{rgb_im_first.shape[0]}, should be 3"
+    assert rgb_im_first.shape[1] == 30, "n_line is not the same"
+
+    rgb_im_last = channel_first_to_channel_last(rgb_im_first)
+    assert rgb_im_last.shape[2] == 3, f"wrong channel number \nis : {rgb_im_last.shape[2]}, should be : 3"
+    assert rgb_im_last.shape[0] == 30, "n_line is not the same"
+
+    # Create image with channel being the last dimension
+    # Then swap for the first
+    # =================================================
+    one_channel_im = np.random.random((30,28))
+    rgb_im_last = to_rgb_channel_last(one_channel_im)
+    assert rgb_im_last.shape[2] == 3, f"wrong channel number \nis :{rgb_im_last.shape[2]}, should be 3"
+    assert rgb_im_last.shape[0] == 30, "n_line is not the same"
+
+    rgb_im_first = channel_last_to_channel_first(rgb_im_last)
+    assert rgb_im_first.shape[0] == 3, f"wrong channel number \nis :{rgb_im_first.shape[0]}, should be 3"
+    assert rgb_im_first.shape[1] == 30, "n_line is not the same"
+
+    print("Tests ok.")
+
+
--- src/maze.py
+++ src/maze.py
@@ -3,9 +3,10 @@ import numpy as np
 from copy import copy
 
 import matplotlib
-matplotlib.use('Qt5Agg')
+#matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
+from image_utils import to_rgb_channel_first, to_rgb_channel_last, channel_last_to_channel_first, channel_first_to_channel_last, plot_single_image
 
 class ImageGridWorld(object):
     def __init__(self, config, show=False):
@@ -20,7 +21,7 @@ class ImageGridWorld(object):
         self.size_img = self.fashion_im[0].shape
         self.size_vector = self.size_img[0]*self.size_img[1]
 
-        self.black_image = np.zeros((*self.size_img, 3))
+        self.black_image = np.zeros((3, *self.size_img))
 
         self.background = []
         self.create_background(show=False)
@@ -95,29 +96,29 @@ class ImageGridWorld(object):
         #A State correspond to : (current image, north, south, east, west) so 5 images => 15 channel
         # (may be too much)
         # TODO : Rescale image ?? Less channel ?
-        self.observation = np.zeros((self.size_img[0], self.size_img[1], 15))
+        self.observation = np.zeros((15, self.size_img[0], self.size_img[1]))
 
-        self.observation[:, :, :3] = self.get_current_square()
+        self.observation[ :3, :, :] = self.get_current_square()
 
         if x+1 < self.n_row : #North
-            self.observation[:, :, 3:6] = self.grid[x+1, y]
+            self.observation[3:6, :, :] = self.grid[x+1, y]
         else:
-            self.observation[:, :, 3:6] = self.black_image
+            self.observation[ 3:6, :, :] = self.black_image
 
         if x-1 >= 0 : #South
-            self.observation[:, :, 6:9] = self.grid[x-1, y]
+            self.observation[6:9, :, :] = self.grid[x-1, y]
         else:
-            self.observation[:, :, 6:9] = self.black_image
+            self.observation[6:9, :, :] = self.black_image
 
         if y-1 >= 0 : #East
-            self.observation[:,:,9:12] = self.grid[x, y-1]
+            self.observation[9:12, :, :] = self.grid[x, y-1]
         else:
-            self.observation[:, :,9:12] = self.black_image
+            self.observation[9:12, :, :] = self.black_image
 
         if y+1 < self.n_col : #West
-            self.observation[:,:, 12:15] = self.grid[x, y+1]
+            self.observation[12:15, :, :] = self.grid[x, y+1]
         else:
-            self.observation[:, :, 12:15] = self.black_image
+            self.observation[12:15, :, :] = self.black_image
 
         return self.observation
 
@@ -132,14 +133,25 @@ class ImageGridWorld(object):
             return 0
 
     def render(self):
+        """
+        This function print the board and the position of the agent
+        ONLY in this function, the image format is (H,W,C) (changed at the beginning)
+        """
+        #Todo : don't recreate the plotted grid everytime, just change the point position
 
         custom_grid = np.copy(self.grid)
+
+        # swap from x,y, c, h, w => x,y, h,w,c
+        custom_grid = custom_grid.swapaxes(2,4)
+        custom_grid = custom_grid.swapaxes(2,3)
+
         if self.position != []:
             x,y = self.position
             x_size, y_size = self.size_img
             x_middle = x_size//2
             y_middle = y_size//2
 
+            # Display agent position as a red point.
             custom_grid[x,y, x_middle-3:x_middle+3, y_middle-3:y_middle+3, :] = [1,0,0]
 
         shown_grid = np.concatenate([custom_grid[i] for i in reversed(range(self.n_row))], axis=1)
@@ -153,12 +165,12 @@ class ImageGridWorld(object):
     def create_grid_of_image(self, grid_type="all_diff", show=False):
 
         if grid_type == "all_diff":
-            self.grid = np.zeros((self.n_row, self.n_col, self.size_img[0], self.size_img[1], 3))
+            self.grid = np.zeros((self.n_row, self.n_col, 3, self.size_img[0], self.size_img[1]))
 
             count = 0
             for i in range(self.n_row):
                 for j in range(self.n_col):
-                    background_color = self.background[i,j]
+                    background_color = self.background[:, i,j]
                     self.grid[i,j] = self.load_random_image_per_class(count, background_color, show=False)
                     count += 1
         else:
@@ -169,10 +181,10 @@ class ImageGridWorld(object):
 
     def create_background(self, show=False):
 
-        background = np.ones((self.n_row, self.n_col, 3))
+        background = np.ones((3, self.n_row, self.n_col))
 
-        background[:, :, 0] = np.tile(np.linspace(0, 1, self.n_col), (self.n_row, 1))
-        background[:, :, 2] = np.tile(np.linspace(1, 0, self.n_row), (self.n_col, 1)).T
+        background[0, :, :] = np.tile(np.linspace(0, 1, self.n_col), (self.n_row, 1))
+        background[2, :, :] = np.tile(np.linspace(1, 0, self.n_row), (self.n_col, 1)).T
 
         self.background = background
 
@@ -192,28 +204,21 @@ class ImageGridWorld(object):
             class_id = class_id - 10
 
         random_image_id = np.random.choice(np.where(labels == class_id)[0])
-        image_selected = dataset[random_image_id]
+        image_selected_channel_last = dataset[random_image_id]
 
         #black_area = np.where(image_selected == 0)
-        black_area = np.where(image_selected < 10)
+        black_area = np.where(image_selected_channel_last < 10)
 
-        image_selected = self.to_rgb(image_selected)
-
-        image_selected[black_area] = background_color
+        image_selected_channel_last = to_rgb_channel_last(image_selected_channel_last)
+        image_selected_channel_last[black_area] = background_color
 
         if show:
             plt.figure()
-            plt.imshow(image_selected)
+            plt.imshow(image_selected_channel_last)
             plt.show()
 
-        return image_selected
+        return channel_last_to_channel_first(image_selected_channel_last)
 
-    def to_rgb(self, im):
-        w, h = im.shape
-        ret = np.empty((w, h, 3), dtype=np.float32)
-        ret[:, :, 0] = im/255
-        ret[:, :, 1] = ret[:, :, 2] = ret[:, :, 0]
-        return ret
 
     def action_space(self):
         return 4
