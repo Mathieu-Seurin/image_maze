dbcd1bd6877f96c18511ea4100fbdfa7732d2540
Seurin Mathieu <matseurin@cegetel.net>
2018-05-18 09:29:36 +0200

    run multiple works, need to add env iteration

--- src/run_multiple.py
+++ src/run_multiple.py
@@ -1,3 +1,4 @@
+import subprocess
 from subprocess import Popen
 from itertools import product
 import os
@@ -7,44 +8,78 @@ from parse_dir import parse_env_subfolder
 env_config = "multi_obj"
 env_ext = "10obj_every2"
 
-model_to_test = ['resnet_dqn', 'dqn_filmed']
-#extension_to_test = ['soft_update0_1', 'soft_update0_01']
+verbose = True
+if verbose:
+    STDOUT = subprocess.STDOUT
+else:
+    STDOUT = subprocess.DEVNULL
+
+env_folder = "config/env_base/"
+env_ext_folder = "config/env_ext/"
+
+model_to_test = ['resnet_dqn_pretrain', 'dqn_filmed_pretrain']
 extension_to_test = ['soft_update0_1', 'soft_update0_01', 'soft_update0_001', 'soft_update0_0001',
                       'hard_update0_1', 'hard_update0_01', 'hard_update0_001']
 
-n_seed = 5
+model_folder = "config/model/"
+model_ext_folder = "config/model_ext/"
+
 n_gpu = 4
+capacity_per_gpu = 4
+n_seed = 5
 
 seeds = [i for i in range(n_seed)]
 
-for seed in seeds:
-    processes = []
+general_command = "python3 src/main.py -env_config {}.json -env_extension {}.json -model_config {}.json -model_extension {}.json -device {} -seed {}"
 
-    for test_num, (model, model_ext) in enumerate(product(model_to_test,extension_to_test)):
+#Fill queue with all your expe
+command_keys = ['model', 'model_ext', 'seed']
+all_commands = [dict(zip(command_keys, command_param)) for command_param in product(model_to_test, extension_to_test, seeds)]
+print("{} experiments to run.".format(len(all_commands)))
+processes = []
 
-        if test_num > 4*n_gpu - 1:
-            "Can't run more than {} expes in parallel".format(n_gpu*4 -1)
-            break
+# Launch expe, fill all processes
+for expe_num in range(n_gpu*capacity_per_gpu):
+    try:
+        command_param = all_commands.pop()
+    except IndexError:
+        break
 
-        device_to_use = test_num%n_gpu
+    model_config = model_folder+command_param['model']
+    model_ext = model_ext_folder+command_param['model_ext']
+    seed = command_param['seed']
+    device_to_use = int(expe_num%n_gpu)
 
-        config_location = 'config/{}/{}'
-        env_config_path = config_location.format("env_base", env_config)
-        env_ext_path = config_location.format("env_ext", env_ext)
+    command = general_command.format(env_folder+env_config, env_ext_folder+env_ext, model_config, model_ext, device_to_use, seed)
+    processes.append(Popen(command, shell=True, env=os.environ.copy(), stderr=STDOUT))
 
-        model_config_path = config_location.format("model", model)
-        model_ext_path = config_location.format("model_ext", model_ext)
+print("{} expes remains at the moment".format(len(all_commands)))
+remains_command = len(all_commands) > 0
+while remains_command:
 
-        command = "python3 src/main.py -env_config {}.json -env_extension {}.json -model_config {}.json -model_extension {}.json -device {} -seed {}".format(
-                    env_config_path, env_ext_path, model_config_path, model_ext_path, device_to_use, seed)
-        print(command)
-        processes.append(Popen(command, shell=True, env=os.environ.copy()))
+    for expe_num, expe in enumerate(processes):
+        if expe.poll() is not None:
+            try:
+                command_param = all_commands.pop()
+                print("Launching new expe, {} expe remains".format(len(all_commands)))
 
-    for p in processes:
-        p.wait()
+            except IndexError:
+                remains_command = False
+                break
 
+            model_config = model_folder + command_param['model']
+            model_ext = model_ext_folder + command_param['model_ext']
+            seed = command_param['seed']
+            device_to_use = int(expe_num % n_gpu)
+
+            command = general_command.format(env_folder + env_config, env_ext_folder + env_ext, model_config, model_ext,
+                                             device_to_use, seed)
+            processes[expe_num] = Popen(command, shell=True, env=os.environ.copy(), stderr=STDOUT)
+
+for expe in processes:
+    expe.wait()
 print('Done running experiments')
 
-out_dir = "out/"+env_config+'_'+env_ext
+out_dir = "out/" + env_config + '_' + env_ext
 
 parse_env_subfolder(out_dir=out_dir)
